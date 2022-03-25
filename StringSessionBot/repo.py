7d8fdfeaf8c20cd8f5209b8b6d1fbeae4ed3518e
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-03-25 15:21:19 +0500

    Session Bot

---
+++ StringSessionBot/repo.py
@@ -0,0 +1,14 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+
+
+# Repo Message
+@Client.on_message(filters.private & filters.incoming & filters.command("repo"))
+async def repo(bot, msg):
+    await bot.send_message(
+        msg.chat.id,
+        Data.REPO,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+    )
