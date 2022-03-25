96c86b982a78c23a14f941dcd9531302e2897c89
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-03-25 15:53:07 +0500

    Session Bot

--- Data.py
+++ Data.py
@@ -15,7 +15,7 @@ If you don't trust this bot,
 Still reading?
 You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
 
-By @AsadSupport And @Alexa_Help
+By @Shayri_Music_Lovers And @AsadSupport
     """
 
     # Home Button
