68f0c2a5bbba9dbd0edc199d09c08412a39ed42f
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-05-21 09:55:33 +0500

    Session Bot

--- string.py
+++ string.py
@@ -1,5 +1,4 @@
 import config
-import logging
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
