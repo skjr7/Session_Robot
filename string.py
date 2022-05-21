5fd8c006a54b96ea76d11b353482c50789377ed4
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-05-21 09:50:47 +0500

    Session Bot

--- string.py
+++ string.py
@@ -4,10 +4,6 @@ from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-
 app = Client(
     "bot",
     api_id=config.API_ID,
