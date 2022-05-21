6016ee27a5a057490509de64738f35b41599e40c
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-05-21 11:07:52 +0500

    Session Bot

--- bot.py
+++ bot.py
@@ -9,11 +9,10 @@ logging.basicConfig(
 ) # type: ignore
 
 app = Client(
-    "bot",
-    api_id=env.API_ID,
-    api_hash=env.API_HASH,
-    bot_token=env.BOT_TOKEN,
-    in_memory=True,
+    ":memory:",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
     plugins=dict(root="StringSessionBot"),
 )
 
